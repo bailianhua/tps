502f9dfcc5ca8c3d4e339f8b265f0307b00c75f0
Bai <yueshendian@gmail.com>
2021-10-02 18:17:36 +0700

    run wild

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="app">
+    <video-background
+      :src="videoBackground.src"
+      :muted="videoBackground.muted"
+      :loop="videoBackground.loop"
+      style="height: 100vh;"
+      overlay="rgba(50, 50, 50, 0.7)"
+    >
+      <p
+        class="underline align-middle text-gray-100 text-center text-5xl tracking-tight"
+      >{{ title }}</p>
+      <p
+        class="align-middle text-gray-100 text-center text-5xl font-black tracking-widest leading-relaxed"
+      >{{ teamTag }}</p>
+      <p
+        class="italic align-middle text-gray-100 text-center text-2xl font-bold tracking-tight"
+      >{{ teamName }}</p>
+    </video-background>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import VideoBackground from 'vue-responsive-video-background-player';
+export default defineComponent({
+  name: 'App',
+  components: { VideoBackground },
+  data() {
+    return {
+      title: "World First Racer",
+      teamTag: "TPS",
+      teamName: "Thoughts Per Second",
+      videoBackground: {
+        src: "/vids/endwalkerLogin.mp4",
+        muted: true,
+        loop: false
+      }
+    }
+  }
+});
+</script>
+
+<style>
+.title {
+  color: #e6e4e4;
+  text-shadow: -0.1em -0.1em 0.1em #8d4909;
+}
+</style>
